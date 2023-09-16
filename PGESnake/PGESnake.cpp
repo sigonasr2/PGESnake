5116cd45caf7cada7cd24aa67349764524d5ea24
WedrownyElite <chris.sliwinski@gmail.com>
2023-09-16 14:11:01 -0500

    Add project files.

---
+++ PGESnake/PGESnake.cpp
@@ -0,0 +1,87 @@
+#define OLC_PGE_APPLICATION
+#include "olcPixelGameEngine.h"
+
+//THIS IS WRITTEN USING OLC GPE, CHECK OUT onelonecoder.com
+//Or else
+class Snake : public olc::PixelGameEngine {
+public:
+	Snake() {
+		sAppName = "Snake";
+	}
+
+	enum direction { STOP, LEFT, RIGHT, DOWN, UP };
+	direction dir;
+	//Game variables
+	int score;
+	//Snake variables
+	int SnakeXPos, SnakeYPos, speed = 1;
+	int tailX[100], tailY[100], tailLength;
+	//Tail coordinates
+	int fposX, fposY, sposX, sposY;
+	//Target variables
+	int targetX, targetY;
+
+
+
+private:
+
+public:
+	bool OnUserUpdate(float fElapsedTime) override {
+		Clear(olc::BLACK);
+		//Draw top border
+		DrawLine(5, 5, ScreenWidth() - 5, 5, olc::WHITE);
+		//Draw left border
+		DrawLine(5, 5, 5, ScreenHeight() - 5, olc::WHITE);
+		//Draw right border
+		DrawLine(ScreenWidth() - 5, 5, ScreenWidth() - 5, ScreenHeight() - 5, olc::WHITE);
+		//Draw bottom border
+		DrawLine(5, ScreenHeight() - 5, ScreenWidth() - 5, ScreenHeight() - 5, olc::WHITE);
+
+		//Draw Snake
+		DrawRect(SnakeXPos, SnakeYPos, 1, 1, olc::GREEN);
+
+		//Directions changes
+		if (GetKey(olc::Key::UP).bPressed && dir != DOWN) {
+			dir = UP;
+		}
+		if (GetKey(olc::Key::DOWN).bPressed && dir != UP) {
+			dir = DOWN;
+		}
+		if (GetKey(olc::Key::LEFT).bPressed && dir != RIGHT) {
+			dir = LEFT;
+		}
+		if (GetKey(olc::Key::RIGHT).bPressed && dir != LEFT) {
+			dir = RIGHT;
+		}
+
+		//Move Snake
+		switch (dir) {
+		case LEFT:
+			SnakeXPos -= speed;
+			break;
+		case RIGHT:
+			SnakeXPos += speed;
+			break;
+		case DOWN:
+			SnakeYPos += speed;
+			break;
+		case UP:
+			SnakeYPos -= speed;
+			break;
+		}
+		return true;
+	}
+
+	bool OnUserCreate() override {
+		dir = STOP;
+
+		return true;
+	}
+};
+
+int main() {
+	Snake demo;
+	if (demo.Construct(300, 300, 2, 2))
+		demo.Start();
+	return 0;
+}
